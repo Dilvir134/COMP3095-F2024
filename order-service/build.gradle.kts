81c44d7e09a48bc4bac7d7e43c379a8527c461b7
Dilvir134 <dilvir134@gmail.com>
2024-11-21 18:36:35 -0500

    completed labs till 14 Nov 2024

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	testImplementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "microservices-parent"
 
-include("product-service", "inventory-service", "order-service")
+include("product-service", "inventory-service", "order-service", "api-gateway")
