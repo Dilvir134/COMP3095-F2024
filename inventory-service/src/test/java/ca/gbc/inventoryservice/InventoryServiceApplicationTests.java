29e47ff2a8b8d4d3f8330ad5404aebc22c17e4aa
Dilvir134 <dilvir134@gmail.com>
2024-12-10 09:48:40 -0500

    completed inventory service integration tests.

--- inventory-service/src/test/java/ca/gbc/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/ca/gbc/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,15 +1,49 @@
 package ca.gbc.inventoryservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.hamcrest.Matchers.is;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("t_inventory")
+            .withUsername("admin")
+            .withPassword("password");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        postgreSQLContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void getInventoryTest() {
+        var responseBodyString = RestAssured.given()
+                .when()
+                .get("/api/inventory?skuCode=SKU001&quantity=10")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(is("true"));
     }
 
 }
